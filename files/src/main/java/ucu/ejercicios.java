18d0b9383e8402405e69718502762fa243ed2eea
Tachoviendo <ignacio.3249@gmail.com>
2026-03-25 19:13:01 -0300

    Creación de ejercicio files. Ejercicio 1 pronto

---
+++ files/src/main/java/ucu/ejercicios.java
@@ -0,0 +1,29 @@
+package ucu;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+
+public class ejercicios {
+    //ejercicio1
+    
+    public static void crearArchivo() {
+        try {
+            FileWriter fw = new FileWriter("saludo.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("Hola");
+            bw.newLine();
+            bw.write("Bienvenidos a Java\n");
+            bw.newLine();
+            bw.write("Manejo de archivos Básico");
+            fw.close();
+
+            System.out.println("Archivo creado Exitosamente!");
+
+        } catch (IOException e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+    }
+    
+}
