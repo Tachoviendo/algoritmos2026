0fbaaeabb7dc2676c448eb129016fbe753805b08
aristov983 <earis4499@gmail.com>
2026-03-28 17:21:21 -0300

    Ejercicio 1 y esturctura del main

--- ejTeam2/src/main/java/general/App.java
+++ ejTeam2/src/main/java/general/App.java
@@ -1,10 +1,41 @@
 package general;
+import java.util.Scanner;
 
-/**
- * Hello world!
- */
+import ejercicios.PruebaAtributos;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("\n==== EJERCICIOS GRUPALES OBLIGATORIOS - ALGORITMOS 1S 2026 ====");
+        Scanner sc = new Scanner(System.in);
+        while (true) { 
+            try {
+                System.out.println("Ingrese un número para ejecutar un ejercicio (1 a 14) o ingrese 0 para salir: ");
+                int opcion = sc.nextInt();
+                switch (opcion) {
+                    case 0:
+                        sc.close();
+                        return;
+                    case 1:
+                        ejercicio1();
+                        break;
+                    default:
+                        System.out.println("Opción invalida.");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Ingresó una opción invalida.");
+            }
+        }
+    }
+
+    public static void ejercicio1(){
+        System.out.println("\n==== EJERCICIO 1 ====");
+        PruebaAtributos pa = new PruebaAtributos();
+        pa.imprimirValoresAtributos();
+        pa.probarNombresVariables();
+        pa.probarVariablesLocales();
+        System.out.println("\n==== FIN EJERCICIO 1 ====");
     }
 }
+
+
