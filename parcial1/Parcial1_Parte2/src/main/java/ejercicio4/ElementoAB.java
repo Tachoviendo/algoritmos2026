d29f169e23ea708218edc0ee00c055f60eece451
tachoviendo <ignacio.3249@gmail.com>
2026-05-25 20:11:58 -0300

    ejercicio 4 done!

--- parcial1/Parcial1_Parte2/src/main/java/ejercicio4/ElementoAB.java
+++ parcial1/Parcial1_Parte2/src/main/java/ejercicio4/ElementoAB.java
@@ -16,14 +16,61 @@ public class ElementoAB {
         this.hijoDer = der;
     }
 
+    private int alturaDeHijo(ElementoAB hijo) {
+      if (hijo == null) {
+          return -1;
+      }
+      return hijo.altura();
+    }
+
     // ====== A IMPLEMENTAR ======
     public int altura() {
-        // TODO
-        return -1;
+
+        int hIzq = alturaDeHijo(hijoIzq);
+        int hDer = alturaDeHijo(hijoDer);
+
+      
+        // veo cual es mayor y sumo 1 por ser la raiz
+        if (hIzq > hDer) {
+            return 1 + hIzq;
+        } else {
+            return 1 + hDer;
+        }
     }
 
     public boolean esAVL() {
-        // TODO
-        return false;
+           
+
+        // caso base
+        if (hijoIzq == null && hijoDer == null) {
+            return true; // nodo hoja = siempre va a ser avl
+        }
+
+      
+        int hIzq = alturaDeHijo(hijoIzq);
+        int hDer = alturaDeHijo(hijoDer);
+        
+        // calculo la diferencia de alturas pq si estan fuera del conjunto = [-1;0;1] no es avl
+        int diferencia = hIzq - hDer;
+        if (diferencia < 0) {
+            //"valor absoluto" para simplificar al condicion del siguiente if.
+            diferencia = diferencia * -1;
+        }
+
+        // si este nodo no esta balanceado, no es AVL
+        if (diferencia > 1) {
+            return false;
+        }
+
+        // ambos subarboles tambien tienen que ser AVL (caso recursivo)
+        //
+        // si el hijo existe y no es avl tengo que retornas false. tanto para izq como para der. 
+        if (hijoIzq != null && !hijoIzq.esAVL()){
+            return false;
+        }
+        if (hijoDer != null && !hijoDer.esAVL()){
+            return false;
+        }
+        return true;
     }
 }
